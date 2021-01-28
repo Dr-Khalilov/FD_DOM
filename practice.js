e556b34404c05ba935214b3d1c0379b7a8a89009
Umar Khalilov <ERMASTER100@gmail.com>
2021-01-28 02:51:14 +0200

    Add corrected codes

--- practice.js
+++ practice.js
@@ -1,19 +1,21 @@
-// 1 Вычислить сумму первых N элементов последовательности . параметр N задает пользователь
-// (например n=4 , 1+2+3+4)
-function sumSequence(n) {
+'use strict';
+// 1 Вычислить сумму первых N элементов последовательности, параметр N задает пользователь (например n=4, 1+2+3+4)
+function calcSumSequence1(num) {
     let sum = 0;
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i <= num; i++) {
         sum += i;
     };
     return sum;
 };
+
+const calcSumSequence2 = (num) => num === 1 ? num : num + calcSumSequence2(num - 1);
 
 // 2.2 Создать объект, который содержит свойства, о факультете и кафедре.
 class EducationalInstitution {
     constructor(faculty, cathedra) {
         this.faculty = faculty;
         this.cathedra = cathedra;
-    }
+    };
 }
 
 // 2.1 Создать объект Student который содержит следующие свойства: имя, фамилию, пол, контактные данные.
@@ -27,27 +29,34 @@ class Student extends EducationalInstitution {
         this.dataContact = dataContact;
     };
 }
+
 const student = new Student('Medical', 'Pediatric', 'John', 'Doe', true, '248734872');
 
 // 2.4 Реализовать функцию выводит на экран всю информацию о студенте
-function getDatesAboutStudent(obj) {
-    // for (let value of Object.entries(obj)) {
-    // let string;
-    // value.forEach(() => {
-    //     string = value.join(': ');
-    // });
-    // console.log(string);
-    // }
+function getDatesAboutStudent1(obj) {
+    for (let value of Object.entries(obj)) {
+        let string;
+        value.forEach(() => {
+            string = value.join(': ');
+        });
+        console.log(string);
+    };
+};
+
+function getDatesAboutStudent2(obj) {
     return Object.entries(obj).map(value => value.join(': ')).join('\n');
-}
-getDatesAboutStudent(student);
+};
+
+getDatesAboutStudent1(student);
+
+console.log(getDatesAboutStudent2(student));
 
 // 3.1 Создать числовой массив и проинициализировать его из 25 элементов.
 // 3.1*Инициализация с помощью случайных чисел
 function createArray(quantity) {
     const arrNums = [];
     for (let i = 0; i < quantity; i++) {
-        arrNums.push(Math.ceil(Math.random() * i));
+        arrNums.push(Math.round(Math.random() * i));
     };
     return arrNums;
 };
@@ -56,24 +65,60 @@ const array = createArray(25);
 console.log(array);
 
 // 3.2 Вывести элементы с четными индексами
+function getEvenIndex(arrOfNums) {
+    let result = [];
+    for (let i = 0; i < arrOfNums.length; i++) {
+        if (i % 2 === 0) {
+            result.push(i);
+        };
+    }
+    return result;
+};
+
+const evenIndex = getEvenIndex(array);
+console.log(evenIndex);
+
 // 3.3 Вывести только четные элементы (четные числа делятся на 2 без остатка)
-// 3.4 Вывести индексы нулевых элементов (элемент равен нулю)
-// 3.5 Подсчитать количество нулевых элементов
 function getEvenNums(arrOfNums) {
     let result = [];
-    let count = 0;
     for (let i = 0; i < arrOfNums.length; i++) {
-        if (arrOfNums[i] % 2 === 0) { // условие должно быть равным нулю чтобы выводились элементы равные 0
-            result.push(arrOfNums[i]); // i если нужно выести индексы
-            count++;
+        if (arrOfNums[i] % 2 === 0) {
+            result.push(arrOfNums[i]);
+        };
+    }
+    return result;
+};
+
+const evenNums = getEvenNums(array);
+console.log(evenNums);
+
+// 3.4 Вывести индексы нулевых элементов (элемент равен нулю)
+function getIndexNullsElements(arrOfNums) {
+    let result = [];
+    for (let i = 0; i < arrOfNums.length; i++) {
+        if (arrOfNums[i] === 0) {
+            result.push(arrOfNums[i]);
         }
     }
-    return result + ' ' + count;
+    return result;
+};
 
-}
+const nullsElem = getIndexNullsElements(array);
+console.log(nullsElem);
 
-const even = getEvenNums(array);
-console.log(even);
+// 3.5 Подсчитать количество нулевых элементов
+function calcQuantityNullsElements(arrOfNums) {
+    let count = 0;
+    for (let i = 0; i < arrOfNums.length; i++) {
+        if (arrOfNums[i] === 0) {
+            count++;
+        };
+    }
+    return count;
+};
+
+const countNullsElem = calcQuantityNullsElements(array);
+console.log(countNullsElem);
 
 // 4 Создать классы:
 // - Книга (автор, название, год издания, издательство)
@@ -100,17 +145,27 @@ class Ebook extends Book {
 // вывод fizzbuzz вместо чисел, кратных как 3, так и 5.
 // вывод fizz вместо чисел, кратных 3;
 // вывод buzz вместо чисел, кратных 5;
-function getOutputNumbersOnConsole(n) {
-    for (let i = 1; i <= n; i++) {
-
-        if (i % 3 === 0) {
-            return `${i} - ${'Fizz'}`;
+function getOutputNumbersOnConsole(num) {
+    const arrFizzBuzz = [];
+    let index = 1;
+    while (index <= num) {
+        arrFizzBuzz.push(index);
+        if (index % 3 === 0) {
+            arrFizzBuzz.pop();
+            arrFizzBuzz.push('Fizz');
         }
-        if (i % 5 === 0) {
-            return `${i} - ${"Buzz"}`;
+        if (index % 5 === 0) {
+            arrFizzBuzz.pop();
+            arrFizzBuzz.push('Buzz');
         }
-        if(i % 3 === 0 && i % 5 === 0){
-            return `${i} - ${"FizzBuzz"}`;
+        if (index % 3 === 0 && index % 5 === 0) {
+            arrFizzBuzz.pop();
+            arrFizzBuzz.push('FizzBuzz');
         }
-    };
+        index++;
+    }
+    return arrFizzBuzz.join('\n');
 };
+
+const fizzBuzz = getOutputNumbersOnConsole(15);
+console.log(fizzBuzz);
